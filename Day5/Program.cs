89f3779dfab69fb6625317c0dc0799113c36441f
Rickard Karlsson <rickard.k@live.com>
2023-12-08 14:14:57 +0100

    working solution for day5 part1 and part2

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Text.RegularExpressions;
 
 class Program
@@ -10,10 +11,12 @@ class Program
 
     static void Main()
     {
-        string fileName = "C:\\Users\\ricka\\dev\\AoC\\Day5\\ExInput.txt";
+        string fileName = "C:\\Users\\ricka\\dev\\AoC\\Day5\\input.txt";
         List<string> fileContent = FileInput(fileName);
-
         Part1(fileContent);
+
+        Part2(fileContent); //dumb bruteforce way to do it, almost 30min runtime
+       
 
     }
 
@@ -21,10 +24,115 @@ class Program
     {
         
         List<long> seeds = GetSeeds(fileContent.First());
-        SortedList<long,long> mappings = GetMappings(fileContent.GetRange(3,2));
+        SortedList<Tuple<long, long>, long> seedToSoilMap = GetMappings(fileContent.GetRange(3, 27));
+        SortedList<Tuple<long, long>, long> soilToFertalizerMap = GetMappings(fileContent.GetRange(32, 20));
+        SortedList<Tuple<long, long>, long> fertalizerToWaterMap = GetMappings(fileContent.GetRange(54, 48));
+        SortedList<Tuple<long, long>, long> waterToLightMap = GetMappings(fileContent.GetRange(104, 42));
+        SortedList<Tuple<long, long>, long> lightToTempMap = GetMappings(fileContent.GetRange(148, 24));
+        SortedList<Tuple<long, long>, long> tempToHumidityMap = GetMappings(fileContent.GetRange(174, 25));
+        SortedList<Tuple<long, long>, long> humidityToLocationMap = GetMappings(fileContent.GetRange(201, 37));
+        List<SortedList<Tuple<long, long>, long>> MapperList = new() { 
+                seedToSoilMap,                                  
+                soilToFertalizerMap,
+                fertalizerToWaterMap, 
+                waterToLightMap, 
+                lightToTempMap, 
+                tempToHumidityMap, 
+                humidityToLocationMap 
+        };
+        long lowestLocation = long.MaxValue;
+
+        foreach (long seed in seeds)
+        {
+            long curr = seed;
+
+
+            foreach (var map in MapperList)
+            {
+                
+                foreach (var range in map) {
+                    long rangeCountFromPos = curr-range.Key.Item1;
+                    
+                    if ( range.Key.Item1 <= curr && curr <= (range.Key.Item1 + (range.Value-1)))
+                    {
+
+                        curr = range.Key.Item2+rangeCountFromPos;
+                        break;
+                    }
+                }
+                
+            }
+            
+            if (curr < lowestLocation)
+            {
+                lowestLocation = curr;
+            }
+            
+        }
+        Console.WriteLine($"PART1 -> lowestLocation: {lowestLocation}");
+    }
+
+    public static void Part2(List<string> fileContent)
+    {
+        List<long> seeds = GetSeeds(fileContent.First());
+
+        SortedList<Tuple<long, long>, long> seedToSoilMap = GetMappings(fileContent.GetRange(3, 27));
+        SortedList<Tuple<long, long>, long> soilToFertalizerMap = GetMappings(fileContent.GetRange(32, 20));
+        SortedList<Tuple<long, long>, long> fertalizerToWaterMap = GetMappings(fileContent.GetRange(54, 48));
+        SortedList<Tuple<long, long>, long> waterToLightMap = GetMappings(fileContent.GetRange(104, 42));
+        SortedList<Tuple<long, long>, long> lightToTempMap = GetMappings(fileContent.GetRange(148, 24));
+        SortedList<Tuple<long, long>, long> tempToHumidityMap = GetMappings(fileContent.GetRange(174, 25));
+        SortedList<Tuple<long, long>, long> humidityToLocationMap = GetMappings(fileContent.GetRange(201, 37));
+        List<SortedList<Tuple<long, long>, long>> MapperList = new() {
+                seedToSoilMap,
+                soilToFertalizerMap,
+                fertalizerToWaterMap,
+                waterToLightMap,
+                lightToTempMap,
+                tempToHumidityMap,
+                humidityToLocationMap };
 
 
 
+
+        long lowestLocation = long.MaxValue;
+
+        for (int i = 0; i < seeds.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j<seeds[(i + 1)]; j++) {
+                    long curr = (seeds[i] + j);
+                    foreach (var map in MapperList)
+                    {
+
+                        foreach (var range in map)
+                        {
+                            long rangeCountFromPos = curr - range.Key.Item1;
+
+                            if (range.Key.Item1 <= curr && curr <= (range.Key.Item1 + (range.Value - 1)))
+                            {
+
+                                curr = range.Key.Item2 + rangeCountFromPos;
+                                break;
+                            }
+                        }
+
+                    }
+                    if (curr < lowestLocation)
+                    {
+                        lowestLocation = curr;
+                        Console.WriteLine(lowestLocation);
+                    }
+                }   
+            }
+
+                
+
+            
+
+        }
+        Console.WriteLine($"PART2 -> lowestLocation: {lowestLocation}");
     }
 
     private static List<long> GetSeeds(string firstLine)
@@ -40,14 +148,13 @@ class Program
         }
         return numbers;
     }
-    private static SortedList<long,long> GetMappings(List<string> mappings)
+    private static SortedList<Tuple<long, long>, long> GetMappings(List<string> mappings)
     {
-        SortedList<long,long> numbers = new SortedList<long, long>();
+        SortedList<Tuple<long, long>, long> dict = new();
         string pattern = @"\b\d+\b";
-        var destinationRangeStart = 0l;
-        var sourceRangeStart = 0l;
-        var rangeLength = 0l;
-        var max = 0l;
+        var destinationRangeStart = 0L;
+        var sourceRangeStart = 0L;
+        var rangeLength = 0L;
         foreach (var line in mappings)
         {
             MatchCollection matches = Regex.Matches(line, pattern);
@@ -56,35 +163,33 @@ class Program
             sourceRangeStart = long.Parse(matches.ElementAt(1).Value);
             rangeLength = long.Parse(matches.ElementAt(2).Value);
 
+            
+            dict.Add(new Tuple<long, long>(sourceRangeStart, destinationRangeStart),  rangeLength);
+
 
         }
 
-        return numbers;
+        return dict;
     }
-    public static void Part2(List<string> fileContent)
-    {
-
-
-    }
-   
-
     
+
+
+
 
     private static List<string> FileInput(string name)
     {
-        List<string> list = new List<string>();
+        List<string> list = [];
 
         try
         {
             // Create a StreamReader to read from the file
-            using (StreamReader rFile = new StreamReader(name))
+            using StreamReader rFile = new(name);
+            // Read each line from the file and add it to the list
+            while (!rFile.EndOfStream)
             {
-                // Read each line from the file and add it to the list
-                while (!rFile.EndOfStream)
-                {
-                    string data = rFile.ReadLine();
+                string? data = rFile.ReadLine();
+                if (data != null)
                     list.Add(data);
-                }
             }
         }
         catch (FileNotFoundException e)
